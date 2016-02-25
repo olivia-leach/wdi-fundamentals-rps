232c9747b43669a07e04ee2c57cf620e04f51f64
Olivia Leach <Olivia@Olivia-Leachs-MacBook-Pro.local>
2016-02-24 23:46:25 -0500

    updated gtWinner function

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,7 +40,21 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if (playerMove == computerMove) {
+        winner = 'tie';
+    } else if (playerMove == 'rock') {
+        if (computerMove == 'scissors') {
+            winner = 'player'
+        } else if (computerMove == 'paper') {
+            winner = 'computer'
+        }
+    } else if (computerMove == 'rock') {
+        if (playerMove == 'scissors') {
+            winner == 'computer'
+        } else if (playerMove == 'paper') {
+            winner == 'player'
+        }
+    }
     return winner;
 }
 
